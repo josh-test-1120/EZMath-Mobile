f6109c7fde827991627c912346cd4c8ccd650f59
Anna Cubine <acubine10@gmail.com>
2024-10-30 10:47:03 -0700

    Made the Calendar button clickable, created a new activity that the calendar button will take the user to, along with the activitys' xml file.

--- app/src/main/java/com/example/ezmathmobile/MainActivity.kt
+++ app/src/main/java/com/example/ezmathmobile/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.ezmathmobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val testManagerBtn: ImageView = findViewById(R.id.testManagerButton)
+        /**
+         * Function to start the test manager activity when calendar button is pressed
+         */
+        testManagerBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@MainActivity, TestManagerActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
