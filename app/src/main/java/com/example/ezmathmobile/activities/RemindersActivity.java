c6025d3e8dde1815a43c8994016a2d1f2c93c20a
Telmen17 <telmen7948@gmail.com>
2024-11-17 22:27:06 -0800

    Fixed the RemindersActivity.java directory and in-progress database query call functionality.

--- app/src/main/java/com/example/ezmathmobile/activities/RemindersActivity.java
+++ app/src/main/java/com/example/ezmathmobile/activities/RemindersActivity.java
@@ -3,8 +3,9 @@
  * show on the screen here.
  * @author Telmen Enkhtuvshin
  */
-package com.example.ezmathmobile;
+package com.example.ezmathmobile.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
@@ -14,6 +15,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ezmathmobile.R;
 import com.example.ezmathmobile.adaptors.ReminderAdapter;
 import com.example.ezmathmobile.adaptors.ReminderGroupAdapter;
 import com.example.ezmathmobile.databinding.ActivityRemindersBinding;
@@ -24,8 +26,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RemindersActivity extends AppCompatActivity {
 
@@ -48,20 +53,20 @@ public class RemindersActivity extends AppCompatActivity {
         List<Reminder> remindersList1 = new ArrayList<>();
 
         Reminder r1 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r1 = new Reminder("R1 test has been successfully scheduled", "green", LocalDateTime.now().minusDays(1));
         }
         remindersList1.add(r1);
 
         Reminder r2 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r2 = new Reminder("You have 1 upcoming test today", "blue", LocalDateTime.now().minusDays(1));
         }
         remindersList1.add(r2);
 
 
         Reminder r3 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r3 = new Reminder("R1 test starts in 15 minutes", "red", LocalDateTime.now().minusDays(1));
         }
         remindersList1.add(r3);
@@ -69,58 +74,66 @@ public class RemindersActivity extends AppCompatActivity {
         // 2nd Reminder list
         List<Reminder> remindersList2 = new ArrayList<>();
         Reminder r4 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r4 = new Reminder("List 2 R1", "green", LocalDateTime.now().minusDays(1));
         }
         remindersList2.add(r4);
 
         Reminder r5 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r5 = new Reminder("List 2 R2", "blue", LocalDateTime.now());
         }
         remindersList2.add(r5);
 
 
         Reminder r6 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             r6 = new Reminder("R1 test starts in 15 minutes", "red", LocalDateTime.now());
         }
         remindersList2.add(r6);
 
 
         ReminderDateBlock rdb1 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             rdb1 = new ReminderDateBlock(remindersList1, LocalDateTime.now().minusDays(1));
         }
         ReminderDateBlock rdb2 = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             rdb2 = new ReminderDateBlock(remindersList2, LocalDateTime.now());
         }
         List<ReminderDateBlock> remindersWithDateList = new ArrayList<>();
         remindersWithDateList.add(rdb1);
         remindersWithDateList.add(rdb2);
 
-//        // Populating with data from the database
-//        FirebaseFirestore database = FirebaseFirestore.getInstance();
-//        database.collection(Constants.KEY_COLLECTION_REMINDERS)
-//                // Getting the query results
-//                .get()
-//                // When the query is successful
-//                .addOnCompleteListener(task -> {
-//                    if (task.isSuccessful() && task.getResult() != null
-//                            && task.getResult().getDocuments().size() > 0) {
-//                        // Iterating over reminder query results
-//                        for (QueryDocumentSnapshot document : task.getResult()) {
-//                            Reminder reminder = new Reminder(document.getString("text"),
-//                                    document.getString("type"));
-//
-//                            // Adding reminder to ArrayList
-//                            remindersList.add(reminder);
-//                        }
-//                    }
-//                    // Removing Progressbar
-//                    loading(false);
-//                });
+        //Creating Hashmap
+        Map<String, List<Reminder>> remindersByDays = new HashMap<>();
+
+        // Populating with data from the database
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_REMINDERS)
+                // Getting the query results
+                .get()
+                // When the query is successful
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null
+                            && task.getResult().getDocuments().size() > 0) {
+                        // Iterating over reminder query results
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Creating individual reminder object
+                            Reminder reminder = null;
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                reminder = new Reminder(document.getString("text"),
+                                        document.getString("type"), LocalDateTime.parse(document.getString("dateTime")));
+                                remindersByDays.computeIfAbsent(reminder.date.format(DateTimeFormatter
+                                        .ofPattern("yyyy-MM-dd")), k -> new ArrayList<>()).add(reminder);
+                            }
+
+                        }
+
+                    }
+                    // Removing Progressbar
+                    loading(false);
+                });
 
 
         //Declaring the Reminder adapter
